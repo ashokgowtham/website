27a77774984ac5e72f1424a77903436b99f4f20a
Ashok Gowtham M <ashokgowtham123@gmail.com>
2014-04-06 18:39:36 +0530

    Ashok | defaults for local. reformat

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Website::Application.config.secret_token = ENV['SECRET_TOKEN']
+Website::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || 'deb21d4967d19705f2d0ac164c2c2759de7fc75df4cab198268387051678bf5b814bc01c9cd58a97f41d97469d9be17f55fb2310b119074cd5e9989a0085a238'
+Website::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'deb21d4967d19705f2d0ac164c2c2759de7fc75df4cab198268387051678bf5b814bc01c9cd58a97f41d97469d9be17f55fb2310b119074cd5e9989a0085a238'
